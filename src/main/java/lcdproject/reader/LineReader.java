f915d2e660f2695df3b4cc2948094f39a051188a
aCastro8585 <jcastro8585@gmail.com>
2019-10-20 10:41:14 -0500

    Created method checkline that checks for strings without a comma, the method passes the first test method

--- src/main/java/lcdproject/reader/LineReader.java
+++ src/main/java/lcdproject/reader/LineReader.java
@@ -7,4 +7,18 @@ public class LineReader {
 
 	}
 
+	/*
+	 * checkLine() checks the next invalid line inputs:
+	 * 1- String without a comma.
+	 */
+	
+	public static String checkLine(String line) {
+		String message = "";
+		int indexOfComma = line.indexOf(',');
+		if (indexOfComma == -1) {                                                       //check for string without comma.
+			message += "Size and number must be separated by a comma.";
+		}
+		return (message.length() == 0) ? line : "Invalid Line Sintax: " + message;      //returns the original line if no errors.
+	}
+
 }
