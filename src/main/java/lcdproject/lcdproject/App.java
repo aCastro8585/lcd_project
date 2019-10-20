75af40dcd6b1639e8d11737c41507303ce7ec792
aCastro8585 <jcastro8585@gmail.com>
2019-10-19 23:33:18 -0500

    Created maven project with Junit dependency

---
+++ src/main/java/lcdproject/lcdproject/App.java
@@ -0,0 +1,13 @@
+package lcdproject.lcdproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
