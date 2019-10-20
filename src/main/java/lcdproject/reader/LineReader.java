1a5fdc7ce8e4afaad2fee44d3b369ead5cce8288
aCastro8585 <jcastro8585@gmail.com>
2019-10-20 17:47:08 -0500

    Added condition to checkLine() to check if size is in range 1-10

--- src/main/java/lcdproject/reader/LineReader.java
+++ src/main/java/lcdproject/reader/LineReader.java
@@ -22,24 +22,26 @@ public class LineReader {
 		if (indexOfComma == -1) { // check for string without comma.
 			message += "Size and number must be separated by a comma.";
 		} else {
-			if (indexOfComma != checkMoreThanOneComma) {  //check for two or more commas.
+			if (indexOfComma != checkMoreThanOneComma) { // check for two or more commas.
 
 				message += "Two or more commas.";
-		    	}
-			else {
-				String[] lineArray= line.split(",");
-	    	    if (lineArray[0].contentEquals("")) { //check for not specified size.
-				       message+= "Specify a Size. ";
-				    }
-	    	    else
-			    {
-				    if (!isNumber.matcher(lineArray[0]).matches()){ //check if size is a number
-				    	message+= "Size not a Number. ";
-				    }
-				   		    
-			    }
+			} else {
+				String[] lineArray = line.split(",");
+				if (lineArray[0].contentEquals("")) { // check for not specified size.
+					message += "Specify a Size. ";
+				} else {
+					if (!isNumber.matcher(lineArray[0]).matches()) { // check if size is a number
+						message += "Size not a Number. ";
+					} else {
+						int size = Integer.parseInt(lineArray[0]); // check if size is in the range
+
+						if (1 > size || size > 10)
+							message += "Size out of range [1-10]. ";
+					}
+
+				}
 			}
-			
+
 		}
 		return (message.length() == 0) ? line : "Invalid Line Sintax: " + message; // returns the original line if no
 																					// errors found.
