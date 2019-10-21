73ccf617ef17d31c2031bad890a957bc38a0bc94
aCastro8585 <jcastro8585@gmail.com>
2019-10-20 23:25:42 -0500

    Created printNumbers method that prints the correct number of rows according to the size specified

--- src/main/java/lcdproject/writer/LCDprinter.java
+++ src/main/java/lcdproject/writer/LCDprinter.java
@@ -15,5 +15,10 @@ public class LCDprinter {
 	public static boolean getSegment (int digit, int segment) {
 		return SEGMENTS[digit][segment];
 	}
+    public static void printNumbers(int size, String number) {
+    	for (int i = 0; i < 2*size+3; i++) {
+    		System.out.print("\n");
+    	}
 
+    }
 }
