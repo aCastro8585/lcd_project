f49aed21b0d1310e66a68d266ab35d06c8cff28d
aCastro8585 <jcastro8585@gmail.com>
2019-10-20 23:23:36 -0500

    Created test to check if the LCDprinter is printing the correct number of rows according to the size

--- src/main/java/lcdproject/writer/LCDprinter.java
+++ src/main/java/lcdproject/writer/LCDprinter.java
@@ -2,16 +2,16 @@ package lcdproject.writer;
 
 public class LCDprinter {
 	private static final boolean[][] SEGMENTS = {{true,true,true,false,true,true,true},
-	          {false,false,true,false,false,true,false},
-	          {true,false,true,true,true,false,true},
-	          {true,false,true,true,false,true,true},
-	          {false,true,true,true,false,true,false},	
-	          {true,true,false,true,false,true,true},	
-	          {true,true,false,true,true,true,true},	
-	          {true,false,true,false,false,true,false},	
-	          {true,true,true,true,true,true,true},	
-	          {true,true,true,true,false,true,true},	
-            };
+										          {false,false,true,false,false,true,false},
+										          {true,false,true,true,true,false,true},
+										          {true,false,true,true,false,true,true},
+										          {false,true,true,true,false,true,false},	
+										          {true,true,false,true,false,true,true},	
+										          {true,true,false,true,true,true,true},	
+										          {true,false,true,false,false,true,false},	
+										          {true,true,true,true,true,true,true},	
+										          {true,true,true,true,false,true,true},	
+									            };
 	public static boolean getSegment (int digit, int segment) {
 		return SEGMENTS[digit][segment];
 	}
--- src/test/java/lcdproject/writer/LCDprinterTest.java
+++ src/test/java/lcdproject/writer/LCDprinterTest.java
@@ -1,7 +1,11 @@
 package lcdproject.writer;
 
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.junit.Test;
 
@@ -27,6 +31,31 @@ public class LCDprinterTest {
 				LCDprinter.getSegment(i,j));
 			}
 		}
+	}
+	
+	@Test
+	public void canPrintCorrectNumberOfRows() {
+		int expectedRows;
+		int rowsDisplayed;
+		ByteArrayOutputStream displayContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(displayContent));
+
+		for (int size = 1; size > 11; size++) {
+			expectedRows = 2 * size + 3;
+			LCDprinter.printNumbers(size, "3");
+			rowsDisplayed = countRows(displayContent.toString());
+			assertEquals(expectedRows, rowsDisplayed);
+		}
+
+	}
+
+	private int countRows(String output) {
+		Matcher m = Pattern.compile("\r\n|\r|\n").matcher(output);
+		int rows = 0;
+		while (m.find()) {
+			rows++;
+		}
+		return rows;
 	}
 
 }
